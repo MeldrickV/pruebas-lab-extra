f6601c10e3fb5ba09318937011c919a624b97b16
MeldrickV <164236913+MeldrickV@users.noreply.github.com>
2026-09-07 19:26:54 -0700

    Change button width to minWidth in MaterialDialog

--- Dialogs/MaterialDialog.cs
+++ Dialogs/MaterialDialog.cs
@@ -50,10 +50,10 @@ namespace LabInventario.Dialogs
                 panel.Children.Add(_numDisponible);
             }
 
-            var btnGuardar = new Button { Content = "Guardar", Classes = { "Flat" }, Width = 90, IsDefault = true };
+            var btnGuardar = new Button { Content = "Guardar", Classes = { "Flat" }, MinWidth = 90, IsDefault = true };
             btnGuardar.Click += async (_, _) => await Guardar();
 
-            var btnCancelar = new Button { Content = "Cancelar", Classes = { "Outlined" }, Width = 90, IsCancel = true };
+            var btnCancelar = new Button { Content = "Cancelar", Classes = { "Outlined" }, MinWidth = 90, IsCancel = true };
             btnCancelar.Click += (_, _) => Close();
 
             var panelBotones = new StackPanel { Orientation = Orientation.Horizontal, Spacing = 10, HorizontalAlignment = HorizontalAlignment.Right, Margin = new Avalonia.Thickness(0, 14, 0, 0) };
